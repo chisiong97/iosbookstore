3b37f94da1d824f2087b3697b79e1aba929d91c8
Chi Siong Foo <chisiong.foo@softspace.com.my>
2020-06-15 13:00:58 +0800

    Initial Commit

---
+++ iosbookstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosbookstore
+//
+//  Created by Foo Chi Siong on 15/06/2020.
+//  Copyright © 2020 Foo Chi Siong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
