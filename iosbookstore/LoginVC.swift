c79b6d3a8a8810b586e11bdf88ed443d8fcb79aa
Chi Siong Foo <chisiong.foo@softspace.com.my>
2020-06-18 16:07:43 +0800

    Refactored code.

--- iosbookstore/LoginVC.swift
+++ iosbookstore/LoginVC.swift
@@ -11,7 +11,7 @@ import RealmSwift
 
 class LoginVC: UIViewController
 {
-
+    
     @IBOutlet weak var txtID: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var btnLogin: UIButton!
@@ -23,35 +23,43 @@ class LoginVC: UIViewController
         print(Realm.Configuration.defaultConfiguration.fileURL)
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func btnLogin(_ sender: UIButton)
     {
         self.performSegue(withIdentifier: "segueLogin", sender: self)
-        
         /*
-        if (txtID.text == "SS" && txtPassword.text == "1111")
-        {
-            print("OK!")
-            self.performSegue(withIdentifier: "segueLogin", sender: self)
-
-        }
-        else
-        {
-           print("Wrong ID or Password!")
-        }
-        */
+         if (txtID.text == "SS" && txtPassword.text == "1111")
+         {
+         print("OK!")
+         txtID.text = ""
+         txtPassword.text = ""
+         self.performSegue(withIdentifier: "segueLogin", sender: self)
+         
+         }
+         else
+         {
+         print("Wrong ID or Password!")
+         let alertController = UIAlertController(title: nil, message: "Wrong ID or Password", preferredStyle: .alert)
+         
+         let okAction = UIAlertAction(title: "OK", style: .cancel, handler: { (alert: UIAlertAction!) in
+         })
+         
+         alertController.addAction(okAction)
+         self.present(alertController, animated: true, completion: nil)
+         }*/
+        
     }
     
     override func viewWillAppear(_ animated: Bool)
     {
-           super.viewWillAppear(animated)
-           navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
     override func viewWillDisappear(_ animated: Bool)
     {
-           super.viewWillDisappear(animated)
-           navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
 }
